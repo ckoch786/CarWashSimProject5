f082b983c3e2c082bea2e7bf5c3ddc0df22c5437
Cory Koch <yunusabd786@gmail.com>
2011-11-13 16:09:25 -0500

    Added javadoc comment to UnboundedQueueInterface and generated Javadoc

--- Project5CarWashSimulation/src/carwashsim/queue/UnboundedQueueInterface.java
+++ Project5CarWashSimulation/src/carwashsim/queue/UnboundedQueueInterface.java
@@ -1,13 +1,18 @@
-//UnboundedQueueInterface.java 			Modified by Cory Koch				Project5
-
-//Interface for a class that implements a queue of T with no bound
-// on the size of the queue. A queue is a "First in, first out" structure.
-
-
 package carwashsim.queue;
-
+/**
+ *UnboundedQueueInterface.java 	&nbsp;&nbsp;&nbsp; Project5
+ *<p>
+ *Interface for a class that implements a queue of T with no bound
+ *on the size of the queue. A queue is a "First in, first out" structure.
+ *</p> 
+ * @author Modified by Cory Koch
+ *
+ * @param <T>
+ */
 public interface UnboundedQueueInterface<T> extends QueueInterface<T> {
+	/**
+	 * 
+	 * @param element Adds element to the rear of this queue.
+	 */
 	void enqueue(T element);
-	// Adds element to the rear of this queue.
-
 }
