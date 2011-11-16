d2303eebe15e1b90e569327cdf170ee97d0b1f93
Cory Koch <yunusabd786@gmail.com>
2011-11-16 16:23:31 -0500

    made some changes to the GUI

--- Project5CarWashSimulation/src/carwashsim/CarWashSimGUI.java
+++ Project5CarWashSimulation/src/carwashsim/CarWashSimGUI.java
@@ -8,68 +8,72 @@ public class CarWashSimGUI {
 	
 		
 	
-	   private static JTextField    hours;
+	   private static JTextField    hours;  
 	   private static JTextField 	minutes;
 	   private static JTextArea     statusTextArea; 
 	   private static JScrollPane   scrollBar;
 
 	   private static class ActionHandler implements ActionListener {
 	      public void actionPerformed(ActionEvent event) {
-		      if (event.getActionCommand().equals("Evaluate")) { 
+		      /*if (event.getActionCommand().equals("Simulate")) { 
 		         int result        = 0;
 				 String errMessage = null;
 		         try {
-		            result = InFixEvaluator.evaluate(expressionText.getText());
-		            statusTextArea.append("Expression = " + expressionText.getText() +
+		            result = InFixEvaluator.evaluate(hours.getText());
+		            statusTextArea.append("Expression = " + hours.getText() +
 		            		                       "\n" + "Result = " + result + "\n");
 				}catch (InFixException error) {        
 		             errMessage = error.getMessage();
 		             statusTextArea.setText("Result = " + errMessage); 
 				}
-		      } else if (event.getActionCommand().equals("Clear")) { 
+				*/
+		       if (event.getActionCommand().equals("Clear")) { 
 		            statusTextArea.setText("");
-		            expressionText.setText("");
+		            hours.setText("");
+		            minutes.setText("");
 		        }
 		 }
 	}
 	 public static void main(String args[]) throws IOException {
 	   JFrame displayFrame = new JFrame();
-	   displayFrame.setTitle("InFix Expression Evaluator Program");
+	   displayFrame.setTitle("Car Wash Simulation Program");
 	   displayFrame.setSize(200,600);
 	   displayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	 
-	   expressionText = new JTextField("Infix expression goes here", 60);
+	   hours          = new JTextField("hours: ", 5);
+	   minutes        = new JTextField("minutes: ", 5); 
 	   statusTextArea = new JTextArea("status", 20,60); 
 	   scrollBar      = new JScrollPane();
 	   statusTextArea.setBorder(new LineBorder(Color.red,3));
 
-	   JButton       evaluate = new JButton("Evaluate");         
+	   JButton       evaluate = new JButton("Simulate");         
 	   JButton       clear    = new JButton("Clear");	       
 	   ActionHandler action   = new ActionHandler();
 	 
 	   evaluate.addActionListener(action);
 	   clear.addActionListener(action);
 
-	   Container contentPane     = displayFrame.getContentPane();
-	   JPanel    expressionPanel = new JPanel();
-	   JPanel    statusPanel     = new JPanel();
-	   JPanel    buttonPanel     = new JPanel();
+	   Container contentPane  = displayFrame.getContentPane();
+	   JPanel    input        = new JPanel();
+	   JPanel    activity     = new JPanel();
+	   JPanel    buttonPanel  = new JPanel();
 	 
-	   expressionPanel.setLayout(new FlowLayout());
-	   expressionPanel.add(expressionText);
-	   statusPanel.add(statusTextArea);
+	   input.setLayout(new FlowLayout());
+	   input.add(hours);
+	   input.add(minutes);
+	   activity.add(statusTextArea);
 
 	   buttonPanel.setLayout(new FlowLayout());
 	   buttonPanel.add(evaluate);
 	   buttonPanel.add(clear);
 
-	   contentPane.add(expressionPanel, "North");
-	   contentPane.add(statusPanel,     "Center");   
-	   contentPane.add(buttonPanel,     "South");
+	   contentPane.add(input,       "North");
+	   contentPane.add(activity,    "Center");   
+	   contentPane.add(buttonPanel, "South");
 	 
 	   displayFrame.pack();
 	   displayFrame.setVisible(true);
 	  }
-}
+	}
 	
 
